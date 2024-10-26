b3557dcd05e53e5321e4cf3b03853d5d40e3301a
Fernand SOUALO <fernand.soualo.ext@allianz-trade.com>
2024-10-26 16:31:53 +0100

    add responsiveness

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,8 +1,10 @@
 import React from "react";
 import { Navbar } from "./Navbar";
+import { Helmet } from "react-helmet";
 
 import "../styles/global.css";
 import { graphql, useStaticQuery } from "gatsby";
+
 export const Layout = ({ children }) => {
   const data = useStaticQuery(graphql`
     {
@@ -16,12 +18,17 @@ export const Layout = ({ children }) => {
   const { copyright } = data.site.siteMetadata;
 
   return (
-    <div className="layout">
-      <Navbar />
-      <div className="content">{children}</div>
-      <footer>
-        <p>{copyright}</p>
-      </footer>
-    </div>
+    <>
+      <Helmet>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </Helmet>
+      <div className="layout">
+        <Navbar />
+        <div className="content">{children}</div>
+        <footer>
+          <p>{copyright}</p>
+        </footer>
+      </div>
+    </>
   );
 };
